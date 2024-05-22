be2aef15801050e69ef0556e6d7dae248377a279
Mateus A <163416539+m44g0@users.noreply.github.com>
2024-05-22 11:59:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V1!')
+  res.send('Testando aplicação NodeJS no Pod na AWS!')
 })
 
 app.get('/status', (req, res) => {
